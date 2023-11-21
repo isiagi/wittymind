c51c884a38a9a7b4fbc1ac3db1dc53e026fd540e
isiagi <isiagigeofrey0@gmail.com>
2023-11-21 13:01:31 +0300

    feature: Make sending emails  work

--- components/cta/Cta.tsx
+++ components/cta/Cta.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+
+import Helper from "@/helpers/Helper";
+import React, { useRef } from "react";
+import { ToastContainer } from "react-toastify";
 
 function Cta() {
+  const formRef = useRef<any>();
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    await Helper(formRef);
+  };
   return (
     <section className="text-gray-600 body-font">
+      <ToastContainer />
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
         <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
           <h1 className="title-font font-medium text-3xl text-gray-900">
@@ -13,32 +25,50 @@ function Cta() {
             hammock starladder roathse. Craies vegan tousled etsy austin.
           </p>
         </div>
-        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+        >
           <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
             Contact Us
           </h2>
           <div className="relative mb-4">
-            <label
-              htmlFor="full-name"
-              className="leading-7 text-sm text-gray-600"
-            >
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
               Full Name
             </label>
             <input
               type="text"
-              id="full-name"
-              name="full-name"
+              id="name"
+              name="name"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-              Email
+            <label
+              htmlFor="telephone"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Telephone
             </label>
             <input
-              type="email"
-              id="email"
-              name="email"
+              type="tel"
+              id="telephone"
+              name="telephone"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="subject"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -61,7 +91,7 @@ function Cta() {
           <p className="text-xs text-gray-500 mt-3">
             Literally you probably haven&apos;t heard of them jean shorts.
           </p>
-        </div>
+        </form>
       </div>
     </section>
   );
